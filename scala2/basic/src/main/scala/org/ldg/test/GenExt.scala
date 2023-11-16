f235e647e8b903320cdc969a975fbf08029c9ace
Lance Gatlin <lg@lancegatlin.org>
2023-11-16 10:32:41 -0600

    updates

---
+++ scala2/basic/src/main/scala/org/ldg/test/GenExt.scala
@@ -0,0 +1,15 @@
+package org.ldg.test
+
+import org.scalacheck.Gen
+
+import scala.reflect.ClassTag
+
+object GenExt {
+  implicit class GentExt[A](val self: Gen[A]) extends AnyVal {
+    def sampleOrFail(implicit aClassTag: ClassTag[A]): A = self.sample.getOrElse(
+      throw new RuntimeException(s"failed to sample Gen[${aClassTag.getClass.getSimpleName}]")
+    )
+    def toLazyList: LazyList[A] =
+      LazyList.continually(sampleOrFail)
+  }
+}
